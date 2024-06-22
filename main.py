0f5852ecae40e8ec4856041c35b76e65b9b9f526
NicoSimo <nicolo.simonato22@gmail.com>
2024-06-22 21:31:00 +0200

    Added periodic ML model training

--- main.py
+++ main.py
@@ -3,32 +3,33 @@ import Consumers.redis_consumer as redis_consumer
 import Consumers.postgre_consumer as postgre_consumer
 import Energy_consumption_sensors.sensor_scheduler as sensor_scheduler
 import Predictor.prediction_processing as prediction_processing
+import Training_scripts.rf_training as rf_training  
 import Setup.buildings_info
 
 def run_all():
-    #Setup
-    Setup.buildings_info.buildings_loader()  #Upload building information to redis
+    Setup.buildings_info.buildings_loader()  # Load building information
 
-    # Start Kafka to Redis consumer
+    # Set up various threads for different components
     kafka_redis_thread = threading.Thread(target=redis_consumer.run_redis_consumer)
-    kafka_redis_thread.start()
-
-    # Start Kafka to PostgreSQL consumer
     kafka_postgre_thread = threading.Thread(target=postgre_consumer.run_postgre_consumer)
-    kafka_postgre_thread.start()
-    #Start Redis to Kafka consumer
     processor_thread = threading.Thread(target=prediction_processing.process)
-    processor_thread.start()
-
-    # Start sensor scheduler
     sensor_thread = threading.Thread(target=sensor_scheduler.run_sensor_scheduler)
-    sensor_thread.start()
+    training_thread = threading.Thread(target=rf_training.run_training)
 
-    # Join threads to wait for their completion
-    sensor_thread.join()
+    # Start all threads
+    kafka_redis_thread.start()
+    kafka_postgre_thread.start()
+    processor_thread.start()
+    sensor_thread.start()
+    training_thread.start()
+
+    # Now join all threads
     kafka_redis_thread.join()
     kafka_postgre_thread.join()
     processor_thread.join()
+    sensor_thread.join()
+    training_thread.join()
 
 if __name__ == '__main__':
     run_all()
+

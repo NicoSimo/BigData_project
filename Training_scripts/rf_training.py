c18e499ef262ec1c88f053d1795513054fb106aa
NicoSimo <nicolo.simonato22@gmail.com>
2024-06-23 21:40:29 +0200

    Fixed file naming to avoid problems with docker images

--- Training_scripts/rf_training.py
+++ Training_scripts/rf_training.py
@@ -23,7 +23,7 @@ def fetch_data(table_name):
     postgre_host = os.getenv('POSTGRES_HOST', 'postgres')
     postgre_user = os.getenv('POSTGRES_USER', 'postgres')
     postgre_password = os.getenv('POSTGRES_PASSWORD', 'Team3')
-    postgre_db = os.getenv('DATABASE_NAME', 'energy_consumption')
+    postgre_db = os.getenv('POSTGRES_DB', 'energy_consumption')
 
     
     try:
